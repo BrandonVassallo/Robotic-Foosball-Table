2fe503d4f7a6ff88cc8d30858e1e9e48a3a22c65
BrandonVassallo <brandonvassallo3@gmail.com>
2026-01-30 14:47:15 -0500

    Continued Testing, Updated the project_info file for setting up a virtual enviroment

--- OpenCV_test_enviroment.py
+++ OpenCV_test_enviroment.py
@@ -5,15 +5,7 @@ import matplotlib as plt
 
 from IPython.display import Image
 
-img = numpy.ones((10,10,1))
 
-print()
-print(img)
-print()
-
-
-print(img.shape)
-print()
 
 def main():
     imag = cv2.imread("Checkerboard_pattern.png")
@@ -23,6 +15,13 @@ def main():
     
     cv2.imshow("TITLE: Checkerboard", imag)
     cv2.waitKey(0)      # Wait until any keyboard input to move onto the next line
+    cv2.destroyAllWindows()
+
+    img = numpy.ones((500,200,1))
+    img2 = numpy.ones((500,300,200))
+
+    cv2.imshow("TITLE: Custom Image", img2)
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 main()
