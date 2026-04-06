859811a5f358781a7520cbdb6e2bff9b87741928
themanofwar124 <supergoalie124@gmail.com>
2026-04-06 15:32:57 -0400

    Created a new file to store laser activities such as recieving, producing, and pew pew

---
+++ Laser_Activities.py
@@ -0,0 +1,4 @@
+#This file is intended to be imported into all files where the laser reciever or producer is used. 
+#It contains two classes, one for reciever, one for producer, and will provide all functionality required
+
+import gpiozero
