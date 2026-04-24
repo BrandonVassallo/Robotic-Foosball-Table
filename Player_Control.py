199f9e8d9266e342c5398f49d1b5b108a8e637fa
themanofwar124 <supergoalie124@gmail.com>
2026-04-24 15:55:22 -0400

    Fleshed out what I could on the player line class.

--- Player_Control.py
+++ Player_Control.py
@@ -15,20 +15,32 @@ class Player_Line:
 
 
         #Init both motors as servos
-        self.linear_motor = gpiozero.AngularServo(move_pin,max_pulse_width=0.0025,min_pulse_width=0.0005, max_angle=90,min_angle=-90)
-        self.rotational_motor = gpiozero.AngularServo(kick_pin, max_pulse_width=0.0025,min_pulse_width=0.0005, max_angle=90,min_angle=-90)
+        self.linear_motor = gpiozero.AngularServo(move_pin,max_pulse_width=0.0025,min_pulse_width=0.0005, max_angle=180,min_angle=0)
+        self.rotational_motor = gpiozero.AngularServo(kick_pin, max_pulse_width=0.0025,min_pulse_width=0.0005, max_angle=180,min_angle=0)
+
+
+    def move(self,percentage):
         
-
-
-
-
-    def move(self,pixel):
-        pass
-
+        #moves the motor to the percentage requested
+        self.linear_motor.angle = percentage*180
 
 
     def kick(self):
+        
+        #Kick should be 60 degrees
+        #Make it follow through the swing in order to get maximum power
+        
         pass
 
+    
+    def post_up(self):
+        
+        self.linear_motor.angle = 90
+
+        #add method to make them stand vertically with rotational motor
+        
+        pass
+
+    
 
 
