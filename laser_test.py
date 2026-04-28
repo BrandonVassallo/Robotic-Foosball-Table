148d5388658a55f390895fe3dafb8f9bc5a49209
BrandonVassallo <brandonvassallo3@gmail.com>
2026-04-28 14:54:12 -0400

    Updated Laser_test.py

--- laser_test.py
+++ laser_test.py
@@ -4,12 +4,14 @@ new = Laser_Activities.Goal(4)
 
 while True:
 
-    if new.is_laser_detected:
+    if new.is_laser_detected():
         print("Laser detected")
     else:
         print("Nothing")
 
-    if new.is_goal:
+    if new.is_goal():
         print("Goal Scored")
+
+
 
     
